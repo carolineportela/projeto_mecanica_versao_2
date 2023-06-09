3f635bbbf640879b5a2bb5620de8aae0a037a893
carolPortela <carol10.portela12@gmail.com>
2023-06-09 14:59:55 -0300

    testar dnv

--- app.js
+++ app.js
@@ -9,7 +9,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { application } = require('express');
 
 //Import do arquivo de configuração das variaveis,constantes e funcoes globais.
 var message = require('./controller/modulo/config.js')
